120b94edeaf0bb49fd649c2bf36cffb82ca5e659
Hamzx47 <khanazam3324@gmail.com>
2023-02-07 08:00:04 +0500

    lib changed

---
+++ lib/category_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+  const CategoryItem({super.key, required this.title, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
