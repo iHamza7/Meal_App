726779f56ac1fdcfd811d999a3d8f0a594acad86
Hamzx47 <khanazam3324@gmail.com>
2023-02-08 10:10:00 +0500

    lib changed

--- lib/meals_screen.dart
+++ lib/meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 
 class MealScreen extends StatelessWidget {
   static const routeName = '/meals-screen';
@@ -9,12 +10,19 @@ class MealScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeal = dummyMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: const Center(
-        child: Text("this"),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeal[index].title);
+        },
+        itemCount: categoryMeal.length,
       ),
     );
   }
