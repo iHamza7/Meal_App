c9a035822c39a96ba3e088e6348c922b235baaf1
Hamzx47 <khanazam3324@gmail.com>
2023-02-09 09:29:57 +0500

    lib changed

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -19,6 +19,34 @@ class MealItem extends StatelessWidget {
 
   void selectmeal() {}
 
+  String get complexityTest {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simple";
+      case Complexity.Challenging:
+        return "Challenging";
+      case Complexity.Hard:
+        return "hard";
+      default:
+        return "unknown";
+    }
+  }
+
+  String get affordabilityTest {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "Affordable";
+
+      case Affordability.Pricey:
+        return "pricey";
+
+      case Affordability.Luxurious:
+        return "luxurious";
+      default:
+        return "unknown";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -31,16 +59,67 @@ class MealItem extends StatelessWidget {
         margin: const EdgeInsets.all(10),
         child: Column(
           children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-              ),
-              child: Image.network(
-                imageUrl,
-                height: 250,
-                width: double.infinity,
-                fit: BoxFit.cover,
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 15,
+                    ),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      Text(complexityTest),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      Text(affordabilityTest),
+                    ],
+                  ),
+                ],
               ),
             )
           ],
