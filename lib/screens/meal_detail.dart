9d74dbdcb9323d212041ad17afeb25bcc905d2e8
Hamzx47 <khanazam3324@gmail.com>
2023-02-11 07:16:08 +0500

    lib changed

---
+++ lib/screens/meal_detail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MealDetail extends StatelessWidget {
+  const MealDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("this is "),
+    );
+  }
+}
