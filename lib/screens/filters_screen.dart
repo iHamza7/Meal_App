a1a1075de68786d86496f40b2f7bcb04667b2773
Hamzx47 <khanazam3324@gmail.com>
2023-02-13 08:44:13 +0500

    lib changed

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,20 +2,52 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
   const FiltersScreen({super.key});
 
   @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  final _vegetarian = false;
+  final _vegan = false;
+  final _lactoseFree = false;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("this is filters screen"),
-      ),
-      drawer: const MainDrawer(),
-      body: const Center(
-        child: Text('hey'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("this is filters screen"),
+        ),
+        drawer: const MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "Adjust your Meals",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                SwitchListTile(
+                  value: _glutenFree,
+                  title: const Text('Gluten-Free'),
+                  subtitle: const Text('Only Gluten-Free meals'),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                )
+              ],
+            ))
+          ],
+        ));
   }
 }
