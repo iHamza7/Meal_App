81c43a40545324e6412c218894e3c13500bc4da5
Hamzx47 <khanazam3324@gmail.com>
2023-02-13 08:29:01 +0500

    lib changed

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   const FiltersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('hey'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("this is filters screen"),
+      ),
+      drawer: const MainDrawer(),
+      body: const Center(
+        child: Text('hey'),
+      ),
     );
   }
 }
